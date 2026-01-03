a225ed27efbad1202c858c0dc5bb87075928bd14
XNTEABDSC <2364400855@qq.com>
2026-01-03 14:19:25 +0800

    unit_attributes_generic_handlers.lua

--- LuaRules/Configs/unit_attributes_generic_handlers.lua
+++ LuaRules/Configs/unit_attributes_generic_handlers.lua
@@ -4,6 +4,7 @@ if not Attributes then
     GG.Attributes=Attributes
 end
 
+
 ---@class AttributesDataHandler
 ---@field fold fun(domainData:table)
 ---@field apply fun()
@@ -14,24 +15,29 @@ end
 
 ---@class AttributesHandlerFactory
 ---@field new fun(unitID:UnitId,unitDefID:UnitDefId):AttributesHandler
+---@field initialize nil|fun()
+---@field handledAttributeNames table<string,boolean>
 
 ---@type list<AttributesHandlerFactory>
 local HandlersFactory=Attributes.HandlersFactory
 if not HandlersFactory then
     HandlersFactory={}
-    local HandlersFiles=VFS.DirList("LuaRules/Configs/UnitAttributeHandlers", "*.lua") or {}
-    for i = 1, #HandlersFiles do
+    local HandlersFile=VFS.DirList("LuaRules/Configs/UnitAttributeHandlers", "*.lua") or {}
+    for i = 1, #HandlersFile do
+
+        Spring.Echo("unit_attributes_generic_handlers.lua: including " .. HandlersFile[i])
+
         ---@type {[string|number]:AttributesHandlerFactory}
-        local HandlersDefs = VFS.Include(HandlersFiles[i])
+        local HandlersDefs = VFS.Include(HandlersFile[i])
         if not HandlersDefs then
-            Spring.Echo("UnitAttributeHandlers file "..HandlersFiles[i].." return nil")
-            break
-        end
-        for key, value in pairs(HandlersDefs) do
-            if type(key)=="number" then
-                HandlersFactory[#HandlersFactory+1]=value
-            else
-                HandlersFactory[key]=value
+            Spring.Echo("UnitAttributeHandlers file "..HandlersFile[i].." return nil")
+        else
+            for key, value in pairs(HandlersDefs) do
+                if type(key)=="number" then
+                    HandlersFactory[#HandlersFactory+1]=value
+                else
+                    HandlersFactory[key]=value
+                end
             end
         end
     end
